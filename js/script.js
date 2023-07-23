fb782ca482862b7195cd182ff85a479614aafc42
Alex Reckard <alexreckard@gmail.com>
2023-07-23 03:00:36 -0500

    a simple html/css page I made back in May 2022

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// i will spend some time updating this 
+
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const form = document.querySelector("#form");
+
+
+
+// const checkEmail = () => {
+//   let valid = false;
+
+//   const email = 
+// };
+
+
+
+
+
+
+// form.addEventListener('submit', function(e) => {
+//   //prevent form from submitting
+
+//   // e.preventDefault();
+// })
